63fa6a27079095600f0e1ca129b3ba67c5fbf723
philfreshman <philipswiezak@gmail.com>
2026-03-08 13:02:18 +0100

    chore: enable React in the codebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,34 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import react from '@astrojs/react';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	output: 'static',
-	prefetch: true,
-	devToolbar: {
-		enabled: false
-	},
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				'diff-wasm': path.resolve(__dirname, './wasm/diff-wasm/pkg')
-			}
-		},
-		build: {
-			minify: 'esbuild',
-			sourcemap: false
-		},
-		optimizeDeps: {
-			exclude: ['diff-wasm']
-		},
-		worker: {
-			format: 'es'
-		}
-	},
+    output: 'static',
+    prefetch: true,
+    devToolbar: {
+        enabled: false
+    },
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                'diff-wasm': path.resolve(__dirname, './wasm/diff-wasm/pkg')
+            }
+        },
+        build: {
+            minify: 'esbuild',
+            sourcemap: false
+        },
+        optimizeDeps: {
+            exclude: ['diff-wasm']
+        },
+        worker: {
+            format: 'es'
+        }
+    },
+
+	integrations: [react()],
 });
